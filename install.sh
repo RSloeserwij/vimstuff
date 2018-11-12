52e502f83d80afe9efcf9f0a2391472da97ac5c0
Roy Sloeserwij <rs@priogen.nl>
2018-11-12 11:23:53 +0100

    ensure submodule up-to-date before install

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+#ENSURE UPDATED VUNDLE.VIM
+git submodules update --init --recursive
+
 #BACKUP OLD FILES
 if [ -e ~/.vimrc ]; then
+	echo "OLD .vimrc FOUND, MOVING TO .vimrc_old"
 	mv ~/.vimrc ~/.vimrc_old
 fi
 
 if [ -d ~/.vim ]; then
+	echo "OLD .vim FOUND, MOVING TO .vim_old"
 	mv ~/.vim ~/.vim_old
 fi
 
